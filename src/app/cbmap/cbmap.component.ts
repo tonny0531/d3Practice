2ec5f0bfe30fba4b826834d23592e24b35498956
JeffZheng <JeffZheng@chipbond.com.tw>
2022-01-21 18:10:28 +0800

    Update Item

--- src/app/cbmap/cbmap.component.ts
+++ src/app/cbmap/cbmap.component.ts
@@ -11,6 +11,19 @@ import { BaseType, ZoomedElementBaseType } from 'd3';
 })
 export class CBMapComponent implements OnInit {
 
+  colors = {
+    'RUN': '#28a745',
+    'CHK': '#e169b4',
+    'IDLE': '#ffc107',
+    'ALARM': '#ff0000',
+    'MBD': '#ff0000',
+    'READY': '#ffff00',
+    'PAUSE': '#c6e0b4',
+    'RPM': '#800080',
+    'WAIT': '#c0c0c0',
+  };
+
+  itemMap: Map<string, any> = new Map<string, any>();
   itemPoint !: RectInfo[];
   itemDatas !: any[];
   svg!: d3.Selection<BaseType, unknown, HTMLElement, any>;
@@ -18,16 +31,19 @@ export class CBMapComponent implements OnInit {
   constructor(private dataService: CbdataService) { }
 
   ngOnInit(): void {
-    this.itemPoint = this.dataService.getData();
-    this.itemDatas = this.dataService.getItemData();
+    this.initItemMap();
+    this.initItemDetail();
     this.svg = d3.select('svg')
       .attr('width', 1623)
       .attr('height', 609);
-
+    this.svg.append('g')
+      .append('rect')
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .attr('fill', 'gray');
     const adaptLabelFontSize = this.adaptLabelFontSize;
     const clickEvent = this.displayData;
     const sourceData = this.itemDatas;
-    const selectData = this.selectData;
     const container: d3.Selection<SVGGElement, unknown, HTMLElement, any> = this.svg.append('g').attr('id', 'container');
     var zoomed = (event: d3.D3ZoomEvent<SVGGElement, any>) => {
       container.attr('transform', event.transform.toString());
@@ -37,11 +53,7 @@ export class CBMapComponent implements OnInit {
       .scaleExtent([1, 4])
       .on('zoom', zoomed);
     this.svg.call(zoom as any);
-    // container.append('g')
-    //   .append('rect')
-    //   .attr('width', 1623)
-    //   .attr('height', 609)
-    //   .attr('fill','gray');
+
     container.append('g')
       .attr('id', 'bg')
       .append('image')
@@ -50,43 +62,61 @@ export class CBMapComponent implements OnInit {
       .attr('height', 609)
       .attr('preserveAspectRatio', 'none');
 
-    container
-      .selectAll('#itemGroup')
-      .data(this.itemPoint)
+    container.append('g')
+      .attr('id', 'itemGroup')
+      .selectAll('g')
+      .data(Array.from(this.itemMap.values()), (d: any) => d.point.ItemId + d.detail.MergedStatus)
       .enter()
-      .call(this.itemGenerator, adaptLabelFontSize, clickEvent,sourceData, this.updateSelectData.bind(this))
+      .call(this.itemGenerator.bind(this), adaptLabelFontSize, clickEvent, sourceData, this.updateSelectData.bind(this))
   }
 
+  initItemMap(): void {
+    this.itemPoint = this.dataService.getData();
+    this.itemPoint.forEach(item => {
+      this.itemMap.set(item.ItemId, { point: item, detail: null });
+    });
+  }
 
+  initItemDetail(): void {
+    this.itemDatas = this.dataService.getItemData();
+    this.itemDatas.forEach(item => {
+      this.itemMap.get(item.EQPMainInfo.EQPId).detail = item;
+    });
+    (globalThis as any).data = this.itemMap;
+  }
 
   itemGenerator(
-    sel: d3.Selection<d3.EnterElement, RectInfo, d3.BaseType, any>,
+    sel: d3.Selection<d3.EnterElement, any, d3.BaseType, any>,
     adapter: () => null | string,
-    clickEvent: (eqpId: string, sourceData: any[], updateSelectData :Function) => null,
-    sourceData: any[], selectData:any): void {
-    const group = sel.append('g')
-      .attr('id', d => d.ItemId)
-      .attr('width', d => +d.Width)
-      .attr('height', d => +d.Height)
-      .attr('cursor','pointer')
-      .on('click', (event : any, d : any) => clickEvent(d.ItemId,sourceData, selectData))
-      .attr('transform', d => { return 'translate(' + d.X + ',' + d.Y + ')'; });
+    clickEvent: (eqpId: string, sourceData: any[], updateSelectData: Function) => null,
+    sourceData: any[], selectData: any): void {
+    const itemGroup = sel.append('g')
+      .attr('class', 'item');
+    const group = itemGroup.append('g')
+      .attr('id', d => d.point.ItemId)
+      .attr('width', d => +d.point.Width)
+      .attr('height', d => +d.point.Height)
+      .attr('cursor', 'pointer')
+      .on('click', (event: any, d: any) => clickEvent(d.point.ItemId, sourceData, selectData))
+      .attr('transform', d => { return 'translate(' + d.point.X + ',' + d.point.Y + ')'; });
     group.append('rect')
-      .attr('width', d => +d.Width)
-      .attr('height', d => +d.Height)
-      .attr('fill', 'pink');
+      .attr('width', d => +d.point.Width)
+      .attr('height', d => +d.point.Height)
+      .attr('rx', 5)
+      .attr('fill', d => {
+        return this.colors[d.detail.MergedStatus as 'RUN' | 'CHK' | 'IDLE' | 'ALARM' | 'MBD' | 'READY' | 'PAUSE' | 'RPM' | 'WAIT'];
+      });
     group.append('text')
-      .text(d => d.ItemId)
+      .text(d => d.point.ItemId)
       .attr('text-anchor', 'middle')
       .attr('font-size', adapter)
-      .attr('dx', d => +d.Width / 2)
-      .attr('dy', d => +d.Height / 2);
+      .attr('dx', d => +d.point.Width / 2)
+      .attr('dy', d => +d.point.Height / 2);
   }
-
-  adaptLabelFontSize(d: RectInfo) {
+  adaptLabelFontSize(d: any) {
     var xPadding, diameter, labelAvailableWidth, labelWidth;
     xPadding = 8;
-    diameter = +d.Width;
+    diameter = +d.point.Width;
     labelAvailableWidth = diameter - xPadding;
 
     labelWidth = (this as any).getComputedTextLength();
@@ -96,15 +126,38 @@ export class CBMapComponent implements OnInit {
     return (labelAvailableWidth / labelWidth) + 'em';
   }
 
-  displayData(eqpId: string, sourceData: any[], updateSelectData: Function){
-    // console.log(eqpId);
-    // console.log(sourceData);
+  displayData(eqpId: string, sourceData: any[], updateSelectData: Function) {
     let selectData = sourceData.find(item => item.EQPMainInfo.EQPId == eqpId);
     updateSelectData(selectData);
-    console.log(selectData);
   }
 
-  updateSelectData(newData: any){
+  updateSelectData(newData: any) {
     this.selectData = newData;
+  }
+  onUpdateDataClick(): void {
+    // let newData = JSON.parse(JSON.stringify(this.itemMap.get('DI3012J')));
+    let newData = this.itemMap.get('DI3012J');
+    // newData = {...newData};
+    // newData.detail = {...newData.detail, MergedStatus: 'RUN'};
+    // newData.detail.MergedStatus = 'RUN';
+    console.log(newData);
+    this.itemMap.set('DI3012J', newData);
+    console.log(this.itemMap.values());
+    const itemGroups = this.svg.select('#container')
+      .selectAll('#itemGroup')
+      .selectAll('.item');
+    console.log(itemGroups
+      .data(Array.from(this.itemMap.values()), (d: any) => d.point.ItemId + d.detail.MergedStatus)
+      .exit()
+      .remove());
+    // itemGroups.selectAll('rect')
+    //   .data(temp, (d: any) => d.point.ItemId + d.detail.MergedStatus)
+    //   .attr('width', d => +d.point.Width)
+    //   .attr('height', d => +d.point.Height)
+    //   .attr('rx', 5)
+    //   .attr('class',d => d.point.ItemId)
+    //   .attr('fill', d => {
+    //     return this.colors[d.detail.MergedStatus as 'RUN' | 'CHK' | 'IDLE' | 'ALARM' | 'MBD' | 'READY' | 'PAUSE' | 'RPM' | 'WAIT'];
+    //   });
   }
 }
